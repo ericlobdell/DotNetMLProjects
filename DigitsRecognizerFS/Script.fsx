f9d3e7b180dc6a5a6962c311db9d52a620e6a2d9
Eric Lobdell <eric@ericlobdell.me>
2015-08-22 15:22:53 -0400

    Finished DigitsRecognizer in F#

---
+++ DigitsRecognizerFS/Script.fsx
@@ -0,0 +1,44 @@
+﻿
+open System.IO
+
+type Observation = { Label:string; Pixels: int[] }
+
+let toObservation (csvData:string) =
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels}
+
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let manhattanDistance ( pixels1, pixels2 ) =
+    Array.zip pixels1 pixels2
+    |> Array.map ( fun (x,y) -> abs (x-y) )
+    |> Array.sum
+
+let train (trainingset:Observation[]) =
+    let classify (pixels:int[]) =
+        trainingset
+        |> Array.minBy (fun x -> manhattanDistance ( x.Pixels, pixels))
+        |> fun x -> x.Label
+    classify
+
+let dataPath =  @"D:\repos\MLProjects\data\"
+let trainingPath = dataPath + "trainingsample.csv"
+let trainingData = reader trainingPath
+let classifier = train trainingData
+
+let validationPath = dataPath + "validationsample.csv"
+let validationData = reader validationPath
+
+validationData
+|> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.) 
+|> printfn "Correct %.3f"
+
+
+
+
+
